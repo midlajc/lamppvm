2e6c635702e3ee1a1373116c74f7c27c2c9ed4de
midlajc <mail.midlajc@gmail.com>
2022-12-31 11:50:57 +0530

    install.sh Update

--- install.sh
+++ install.sh
@@ -3,14 +3,18 @@
         type "$1" >/dev/null 2>&1
     }
 
+    lamppvm_echo() {
+        command printf %s\\n "$*" 2>/dev/null
+    }
+
     if [ -z "${BASH_VERSION}" ] || [ -n "${ZSH_VERSION}" ]; then
         # shellcheck disable=SC2016
         lamppvm_echo >&2 'Error: the install instructions explicitly say to pipe the install script to `bash`; please follow them'
         exit 1
     fi
 
-    lamppvm_echo() {
-        command printf %s\\n "$*" 2>/dev/null
+    lamppvm_grep() {
+        GREP_OPTIONS='' command grep "$@"
     }
 
     lamppvm_grep() {
@@ -43,9 +47,9 @@
         local LAMPPVM_SOURCE_URL
         LAMPPVM_SOURCE_URL="$LAMPPVM_SOURCE"
         if [ "_$LAMPPVM_METHOD" = "_script-lamppvm-exec" ]; then
-            LAMPPVM_SOURCE_URL="https://raw.githubusercontent.com/${LAMPPVM_GITHUB_REPO}/${LAMPPVM_VERSION}/lamppvm-exec"
+            LAMPPVM_SOURCE_URL="https://raw.githubusercontent.com/${LAMPPVM_GITHUB_REPO}/${LAMPPVM_VERSION}/lamppvm-exec.sh"
         elif [ "_$LAMPPVM_METHOD" = "_script-lamppvm-bash-completion" ]; then
-            LAMPPVM_SOURCE_URL="https://raw.githubusercontent.com/${LAMPPVM_GITHUB_REPO}/${LAMPPVM_VERSION}/bash_completion"
+            LAMPPVM_SOURCE_URL="https://raw.githubusercontent.com/${LAMPPVM_GITHUB_REPO}/${LAMPPVM_VERSION}/bash_completion.sh"
         elif [ -z "$LAMPPVM_SOURCE_URL" ]; then
             if [ "_$LAMPPVM_METHOD" = "_script" ]; then
                 LAMPPVM_SOURCE_URL="https://raw.githubusercontent.com/${LAMPPVM_GITHUB_REPO}/${LAMPPVM_VERSION}/lamppvm.sh"
@@ -59,20 +63,79 @@
         lamppvm_echo "$LAMPPVM_SOURCE_URL"
     }
 
+    lamppvm_try_profile() {
+        if [ -z "${1-}" ] || [ ! -f "${1}" ]; then
+            return 1
+        fi
+        lamppvm_echo "${1}"
+    }
+
+    #
+    # Detect profile file if not specified as environment variable
+    # (eg: PROFILE=~/.myprofile)
+    # The echo'ed path is guaranteed to be an existing file
+    # Otherwise, an empty string is returned
+    #
+    lamppvm_detect_profile() {
+        if [ "${PROFILE-}" = '/dev/null' ]; then
+            # the user has specifically requested NOT to have lamppvm touch their profile
+            return
+        fi
+
+        if [ -n "${PROFILE}" ] && [ -f "${PROFILE}" ]; then
+            lamppvm_echo "${PROFILE}"
+            return
+        fi
+
+        local DETECTED_PROFILE
+        DETECTED_PROFILE=''
+
+        if [ "${SHELL#*bash}" != "$SHELL" ]; then
+            if [ -f "$HOME/.bashrc" ]; then
+                DETECTED_PROFILE="$HOME/.bashrc"
+            elif [ -f "$HOME/.bash_profile" ]; then
+                DETECTED_PROFILE="$HOME/.bash_profile"
+            fi
+        elif [ "${SHELL#*zsh}" != "$SHELL" ]; then
+            if [ -f "$HOME/.zshrc" ]; then
+                DETECTED_PROFILE="$HOME/.zshrc"
+            elif [ -f "$HOME/.zprofile" ]; then
+                DETECTED_PROFILE="$HOME/.zprofile"
+            fi
+        fi
+
+        if [ -z "$DETECTED_PROFILE" ]; then
+            for EACH_PROFILE in ".profile" ".bashrc" ".bash_profile" ".zprofile" ".zshrc"; do
+                if DETECTED_PROFILE="$(lamppvm_try_profile "${HOME}/${EACH_PROFILE}")"; then
+                    break
+                fi
+            done
+        fi
+
+        if [ -n "$DETECTED_PROFILE" ]; then
+            lamppvm_echo "$DETECTED_PROFILE"
+        fi
+    }
+
     install_lamppvm_from_git() {
         local INSTALL_DIR
         INSTALL_DIR="$(lamppvm_install_dir)"
         local LAMPPVM_VERSION
         LAMPPVM_VERSION="${LAMPPVM_INSTALL_VERSION:-$(lamppvm_latest_version)}"
+        # Check if version is an existing ref
+        if ! command git ls-remote "$(lamppvm_source "git")" "$LAMPPVM_VERSION" | lamppvm_grep -q "$LAMPPVM_VERSION"; then
+            lamppvm_echo >&2 "Failed to find '$LAMPPVM_VERSION' version."
+            exit 1
+        fi
 
         local fetch_error
         if [ -d "$INSTALL_DIR/.git" ]; then
             # Updating repo
             lamppvm_echo "=> lamppvm is already installed in $INSTALL_DIR, trying to update using git"
             command printf '\r=> '
-            fetch_error="Failed to update lamppvm , run 'git fetch' in $INSTALL_DIR yourself."
+            fetch_error="Failed to update lamppvm with $LAMPPVM_VERSION, run 'git fetch' in $INSTALL_DIR yourself."
         else
-            fetch_error="Failed to fetch origin. Please report this!"
+            fetch_error="Failed to fetch origin with $LAMPPVM_VERSION. Please report this!"
             lamppvm_echo "=> Downloading lamppvm from git to '$INSTALL_DIR'"
             command printf '\r=> '
             mkdir -p "${INSTALL_DIR}"
@@ -96,15 +159,15 @@
             fi
         fi
         # Try to fetch tag
-        if command git --git-dir="$INSTALL_DIR"/.git --work-tree="$INSTALL_DIR" fetch origin --depth=1 2>/dev/null; then
+        if command git --git-dir="$INSTALL_DIR"/.git --work-tree="$INSTALL_DIR" fetch origin tag "$LAMPPVM_VERSION" --depth=1 2>/dev/null; then
             :
         # Fetch given version
-        elif ! command git --git-dir="$INSTALL_DIR"/.git --work-tree="$INSTALL_DIR" fetch origin --depth=1; then
+        elif ! command git --git-dir="$INSTALL_DIR"/.git --work-tree="$INSTALL_DIR" fetch origin "$LAMPPVM_VERSION" --depth=1; then
             lamppvm_echo >&2 "$fetch_error"
             exit 1
         fi
         command git -c advice.detachedHead=false --git-dir="$INSTALL_DIR"/.git --work-tree="$INSTALL_DIR" checkout -f --quiet FETCH_HEAD || {
-            lamppvm_echo >&2 "Failed to checkout. Please report this!"
+            lamppvm_echo >&2 "Failed to checkout the given version $LAMPPVM_VERSION. Please report this!"
             exit 2
         }
         if [ -n "$(command git --git-dir="$INSTALL_DIR"/.git --work-tree="$INSTALL_DIR" show-ref refs/heads/master)" ]; then
@@ -127,6 +190,7 @@
     }
 
     lamppvm_do_install() {
+        local METHOD='dev'
         if [ -n "${LAMPPVM_DIR-}" ] && ! [ -d "${LAMPPVM_DIR}" ]; then
             if [ -e "${LAMPPVM_DIR}" ]; then
                 lamppvm_echo >&2 "File \"${LAMPPVM_DIR}\" has the same name as installation directory."
@@ -141,11 +205,15 @@
             fi
         fi
 
-        if lamppvm_has git; then
-            install_lamppvm_from_git
-        else
-            lamppvm_echo >&2 'You need git to install lamppvm'
-            exit 1
+        if [ -z "${METHOD}" ]; then
+            if lamppvm_has git; then
+                install_lamppvm_from_git
+            else
+                lamppvm_echo >&2 'You need git to install lamppvm'
+                exit 1
+            fi
+        elif [ "${METHOD}" = 'dev' ]; then
+            :
         fi
     }
 
